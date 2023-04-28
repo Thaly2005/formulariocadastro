cab225f160694b97e42ab12bb1a092e9240d911b
Thalyta das Neves Ferreira <thalyta.ferreira@alunoescolassociais.org.br>
2023-04-28 12:13:18 -0300

    finaliza projeto

---
+++ cadastro.js
@@ -0,0 +1,10 @@
+function Validar(){
+ let nome = document.getElementById('nome').value;
+
+ if(!nome){
+  alert("Campo de preenchimento obrigatório");
+ }else{
+  alert("Campos preenchidos com sucesso");
+ }
+}
+  
